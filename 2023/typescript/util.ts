f99d2b07ccf8841dbf443c9be6a860d24334f9e7
taxi <renzhentaxibaerde@gmail.com>
2023-12-21 22:22:08 -0500

    allow filtering input

--- 2023/typescript/util.ts
+++ 2023/typescript/util.ts
@@ -3,15 +3,22 @@ import { resolve, basename } from "path";
 
 export async function getInputs() {
   const prefix = basename(process.argv[1], ".ts") + "_";
+  const inputNameWhitelist = process.argv
+    .slice(2)
+    .map((name) => (name.endsWith(".txt") ? name : name + ".txt"));
   const root = resolve(__dirname, "../input");
   const filepaths = await readdir(root);
   const inputs: [string, string][] = [];
   for (const filepath of filepaths) {
     if (filepath.startsWith(prefix)) {
-      inputs.push([
-        filepath.replace(prefix, ""),
-        await readFile(resolve(root, filepath), "utf8"),
-      ]);
+      const name = filepath.replace(prefix, "");
+      if (
+        inputNameWhitelist.length > 0 &&
+        !inputNameWhitelist.includes(`${name}`)
+      ) {
+        continue;
+      }
+      inputs.push([name, await readFile(resolve(root, filepath), "utf8")]);
     }
   }
   return inputs;
@@ -24,7 +31,6 @@ export async function solve<TInput>(
   const inputs = await getInputs();
   for (const [inputName, input] of inputs) {
     const parsedInput = inputParser(input);
-
     console.log("=====", inputName, "=====");
     for (const [name, solver] of Object.entries(solvers)) {
       console.log(`${name}: ${solver(parsedInput)}`);
